fdd4069850afad9211a9585942cd082b1628bddd
felipe-coletti <felipecoletti2@gmail.com>
2025-05-14 18:56:04 -0300

    chore: restructure project folders by moving styles to src/styles and scripts to src/scripts

---
+++ src/scripts/app.js
@@ -0,0 +1,47 @@
+import { audioPlayer, formatTime, isValidId, updateTrackInfo, updatePlayerVisibility } from './utils.js'
+import { renderPlaylist } from './playlist.js'
+import { playlistState, loadPlaylist } from './playlistState.js'
+import { currentTimeDisplays, totalTimeDisplays, setupControlEvents, setupAudioEvents } from './controls.js'
+import { setDisplayVolume, trackState } from './trackState.js'
+
+const fetchPlaylist = () => {
+	loadPlaylist()
+	renderPlaylist()
+}
+
+const loadVolume = () => {
+	audioPlayer.loop = trackState.isLooping
+
+	if (!trackState.isMuted) {
+		setDisplayVolume(trackState.volume)
+	} else {
+		setDisplayVolume(0)
+	}
+}
+
+const initApp = () => {
+	fetchPlaylist()
+	loadVolume()
+	setupControlEvents()
+	setupAudioEvents()
+
+	if (playlistState.currentTrackId && isValidId(playlistState.currentTrackId)) {
+		const track = playlistState.playlist.find(track => track.id === playlistState.currentTrackId)
+
+		updateTrackInfo(track)
+
+		if (!audioPlayer.src) {
+			audioPlayer.src = track.src
+		}
+
+		currentTimeDisplays.forEach(display => {
+			display.textContent = formatTime(0, 'short')
+		})
+		totalTimeDisplays.forEach(display => {
+			display.textContent = formatTime(0, 'short')
+		})
+		updatePlayerVisibility()
+	}
+}
+
+document.addEventListener('DOMContentLoaded', initApp)
